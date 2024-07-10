a6698d9fdfcc6e58d1c7501d6f83a1abb0384a80
estoledo1 <estoledo1@utpl.edu.ec>
2024-07-10 15:48:01 -0500

    clase52bim

--- EjemplosSOLID/EjemplosSOLID/src/paquete06l/Principal.java
+++ EjemplosSOLID/EjemplosSOLID/src/paquete06l/Principal.java
@@ -9,15 +9,22 @@ package paquete06l;
  */
 public class Principal {
     public static void main(String[] args) {
+        TarjetaCredito t1 = new TarjetaCredito ("VISA","11505785648");
+        t1.establecerNombre("Visa");
+        t1.establecerNumero("11505785648");
+        // podemos establecer sus los datos de un objeto por medio de su 
+        // constructor osea enviando directmente, como tambien llamar los 
+        // metodos esablecer de cada tipo de dato 
         
-        MayorEdad representante = new MayorEdad("José", "Visa");
+        MayorEdad representante = new MayorEdad("José", t1);
         
         MenorEdad menor = new MenorEdad("Felipe", representante);
         
         
-        System.out.printf("Nombre:%s - Tarjeta:%s\n",
+        System.out.printf("Nombre:%s - Tarjeta:%s\nNombre:%s - Tarjeta:%s\n",
                 menor.obtenerNombre(), 
-                menor.obtenerRepresentante().obtenerTarjeta());
+                menor.obtenerRepresentante().obtenerTarjeta().obtenerNombre(),
+                representante.obtenerNombre(),representante.obtenerTarjeta());
         
     }
 }
---
+++ EjemplosSOLID/EjemplosSOLID/src/paquete06l/TarjetaCredito.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete06l;
+
+/**
+ *
+ * @author utpl
+ */
+public class TarjetaCredito {
+
+    private String nombre;
+    private String numero;
+
+    public TarjetaCredito(String n, String num) {
+        nombre = n;
+        numero = num;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public void establecerNombre(String n) {
+        nombre = n;
+    }
+
+    public String obtenerNumero() {
+        return numero;
+    }
+
+    public void establecerNumero(String num) {
+        numero = num;
+    }
+
+}
