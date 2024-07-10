ebd22bb80bb280d255058be17338e7379b8e250e
estoledo1 <estoledo1@utpl.edu.ec>
2024-07-10 16:30:52 -0500

    API

--- EjemplosSOLID/EjemplosSOLID/src/paquete10d/Principal.java
+++ EjemplosSOLID/EjemplosSOLID/src/paquete10d/Principal.java
@@ -5,26 +5,46 @@
 package paquete10d;
 
 public class Principal {
+
     public static void main(String[] args) {
-        
+
         APINetflix api = new APINetflix();
         api.establecerApiKey("123455");
-        
+
         GeneradorPeliculas gp = new GeneradorPeliculas();
         gp.establecerLlave(api);
         gp.establecerUrl("http://api.movie?api=");
         System.out.println(gp.obtenerUrl());
-        
+
         System.out.println("---------------------------");
-        
+
         APIAmazonMovie api2 = new APIAmazonMovie();
         api2.establecerApiKey("123455");
-        
+
         GeneradorPeliculas gp2 = new GeneradorPeliculas();
         gp2.establecerLlave(api2);
         gp2.establecerUrl("http://api.movie?api=");
         System.out.println(gp2.obtenerUrl());
-        
-        
+
+        System.out.println("---------------------------");
+
+        APIDisneyPlus api3 = new APIDisneyPlus();
+        api2.establecerApiKey("879451321");
+
+        GeneradorPeliculas gp3 = new GeneradorPeliculas();
+        gp2.establecerLlave(api2);
+        gp2.establecerUrl("http://api.movie?apidisney+=");
+        System.out.println(gp2.obtenerUrl());
+
+           System.out.println("---------------------------");
+
+        APIDgo api4 = new APIDgo();
+        api2.establecerApiKey("258748");
+
+        GeneradorPeliculas gp4 = new GeneradorPeliculas();
+        gp2.establecerLlave(api2);
+        gp2.establecerUrl("http://api.movie?apidgo=");
+        System.out.println(gp2.obtenerUrl());
+
     }
 }
